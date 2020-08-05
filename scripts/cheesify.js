13fd2d8ac803738174b4930b054168a7621584a5
zach chung <chungchunluk@gmail.com>
2020-08-05 19:01:03 +0800

    cheesify extension

--- scripts/cheesify.js
+++ scripts/cheesify.js
@@ -3,9 +3,17 @@
 // ======================================================= //
 
 // TODO: Write a function to listen for messages on the content page using chrome.runtime.onMessage
-
+chrome.runtime.onMessage.addListener(
+  function(request, sender, sendResponse) {
+    // actions based on the request (which corresponds to the object we sent in our message)
+    if (request.action === 'cheesify') cheesify();
+  }
+);
 
 // TODO: Add the image replacement script below
 function cheesify() {
-
+  document.querySelectorAll('img').forEach( (img) => {
+    img.src = `https://source.unsplash.com/${img.width}x${img.height}/?cheese&${Math.random()}`;
+    img.srcset = img.src;
+  })
 }
--- scripts/popup.js
+++ scripts/popup.js
@@ -4,10 +4,13 @@
 
 // TODO: Write a function to send a message to the active tab to 'cheesify' it
 function sendCheesifyMsg() {
+  chrome.tabs.query({active: true, currentWindow: true}, function(tabs) { // Finds tabs that are active in the current window
+    chrome.tabs.sendMessage(tabs[0].id, { action: 'cheesify' }); // Sends a message (object) to the first tab (tabs[0])
+  });
 }
 
 // TODO: Add an event listener to trigger the function above when clicking the 'Cheesify' button
-
+document.getElementById('cheesify').addEventListener('click', event => sendCheesifyMsg());
 
 // ======================================================= //
 
